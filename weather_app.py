21d555effa43211c1603b0730a3956c62244ee0f
user <you@example.com>
2017-12-07 04:05:00 +0300

    Light changes

--- weather_app.py
+++ weather_app.py
@@ -1,3 +1,4 @@
+import os
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 
@@ -8,14 +9,14 @@ class TelegramBot(object):
     updater = Updater(TOKEN)
     def __init__(self, token, port):
         self.updater = Updater(token)
-        self.start_webhook(lister="0.0.0.0", port=port, url_path="telegram")
+        self.updater.start_webhook(listen="0.0.0.0", port=port, url_path="telegram")
         self.dispatcher = self.updater.dispatcher
         self.add_dispatchers()
         self.updater.bot.set_webhook("https://pure-refuge-87808/telegram/")
         self.updater.idle()
     
     def add_dispatchers(self):
-        self.dispatcher.add_handler(CommandHandler('start', self.echo)
+        self.dispatcher.add_handler(CommandHandler('start', self.start))
 
     def start(self, bot, update):
         bot.send_message(chat_id=update.message.chat_id, text="What's up?")
