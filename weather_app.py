3870fbf374f2fee3b31cdddd8a30788574a7a8ef
user <you@example.com>
2017-12-07 04:19:20 +0300

    WE

--- weather_app.py
+++ weather_app.py
@@ -6,19 +6,22 @@ TOKEN = "391437242:AAG3xkWlG1O4MGKAQu_zWVhUp07TAiRuwfw"
 PORT = int(os.environ.get("PORT", "5000"))
 
 class TelegramBot(object):
-    updater = Updater(TOKEN)
+
     def __init__(self, token, port):
         self.updater = Updater(token)
-        self.updater.start_webhook(listen="0.0.0.0", port=port, url_path="telegram")
+        self.updater.start_webhook(listen="0.0.0.0", port=port, url_path=token)
         self.dispatcher = self.updater.dispatcher
         self.add_dispatchers()
-        self.updater.bot.set_webhook("https://pure-refuge-87808.herokuapp.com/telegram/")
+        self.updater.bot.set_webhook("https://pure-refuge-87808.herokuapp.com/" + token)
+        print("INIT")
         self.updater.idle()
     
     def add_dispatchers(self):
+        print("DISPATCHERS")
         self.dispatcher.add_handler(CommandHandler('start', self.start))
 
     def start(self, bot, update):
+        print("START")
         bot.send_message(chat_id=update.message.chat_id, text="What's up?")
         
 if __name__ == "__main__":
